a460ee475f3b748782bc27b46126aefe0ad8e2fd
Nick Mandel <nrmandel@ucdavis.edu>
2015-02-13 10:39:15 -0800

    removes data transferring code and cleans main.js.. only local screenshots functional now

--- main.js
+++ main.js
@@ -42,27 +42,6 @@ $(document).ready(function() {
     $('#'+options.callerID).replaceWith('<video id="remoteVideo" autoplay></video>');
   });
 
-
-
-  var chunkArr = [];
-  
-  comm.on('data', function(options) {
-    var data = options.data;
-    chunkArr.push(data.message);
-    // console.log('options.message is', options.message);
-    console.log(data.last);
-    if (options.data.last) {
-      context2.fillRect(0, 0, width, height);
-      context2.drawImage(video2, 0, 0, width, height);
-      console.log('chunkarr', chunkArr);
-      $('#img2').attr('src', chunkArr.join(''));
-    }
-
-    // console.log('data sent', options.data);
-    
-  })
-
-  var chunkLength = 1000;
   
   localVideo.addEventListener('loadedmetadata', function() {
     ratio = localVideo.videoWidth / localVideo.videoHeight;
@@ -79,123 +58,45 @@ $(document).ready(function() {
       var dataUrl = canvas1.toDataURL('image/webp');
       console.log(dataUrl);
       $('#img1').attr('src', dataUrl);
-      function onRead (event, text) {
-        var data = {}; // obj to transmit over icecomm channel
-        // if (event) {
-        //   text = event.target.result;
-        // }
-        if (text.length > chunkLength) {
-          data.message = text.slice(0, chunkLength);
-        }
-        else {
-          data.message = text;
-          data.last = true;
-        }
-        comm.send(data);
-        var remainingDataUrl = text.slice(data.message.length);
-        console.log(data.last, remainingDataUrl.length);
-        if (remainingDataUrl.length) {
-          setTimeout(function() {
-            onRead(null, remainingDataUrl); 
-          }, 350);
-        }
-      }
-      onRead(true, dataUrl);
     }
   })
-
-  // var errorCallback = function(e) {
-  //   console.log('Reeeejected!', e);
-  // };
-
-  // // vendor prefix
-  // window.URL = window.URL || window.webkitURL;
-  // navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-  //                           navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-  // var video = $('video');
-  // if (navigator.getUserMedia) {
-  //   // var webcam = document.getElementById('pic-btn');
-  //   console.log('getusermedia good to go');
-  //   navigator.getUserMedia({video: true, audio: false}, 
-  //     function(stream) {
-  //       console.log('video', video[0]);
-  //       video[0].src = window.URL.createObjectURL(stream);
-  //     }, errorCallback)
-  // } else {
-  //   console.log('cannot get getusermedia');
-  //   video.src = 'somevideo.webm';
-  //   hideInputButton('.webcam');
-  // }
-
- // function pic () {
- //    console.log('CLICKED!');
- //    $('.camera-modal').show();
- //    navigator.getUserMedia({
- //        video: true,
- //        audio: false
- //      },
- //      function(localMediaStream) {
- //        var video = $('.camera-modal video').get(0);
- //        var cameraModal = container.find('.camera-modal');
-
- //        var modalClose = function() {
- //          $(video).hide();
- //          localMediaStream.stop();
- //          cameraModal.hide();
- //          container.find('.capture').hide();
- //          cameraModal.unbind('click');
- //        };
- //        cameraModal.click(modalClose);
-
- //        video.src = window.URL.createObjectURL(localMediaStream);
- //        video.onerror = function() {
- //          localMediaStream.stop();
- //          modalClose();
- //        };
-
- //        $([container.find('.capture').get(0), video]).
- //          show().
- //          unbind('click').
- //          click(function() {
- //            startLoading();
- //            var scale = Math.min(width / video.videoWidth, height / video.videoHeight, 1);
- //            // draw video on to canvas1
- //            var tmpCanvas = document.createElement('canvas');
- //            tmpCanvas.height = video.videoHeight * scale;
- //            tmpCanvas.width = video.videoWidth * scale;
- //            tmpCanvas.getContext('2d').drawImage(video, 0, 0, video.videoWidth * scale, video.videoHeight * scale);
-
- //            detect(tmpCanvas.toDataURL('image/jpeg'), true);
- //            modalClose();
- //            return false;
- //          });
-
- //      },
- //      function() {
- //        $('.camera-modal').hide();
- //        showStatus(messages.NO_CAMERA);
- //        hideInputButton('.webcam');
- //      }
- //    );
- //    return false;
- //  };
-
-  // $('#pic-btn').click(pic);
-
-
-
-
-  // api.request('detection/detect', {
-  //   url: 'http://cn.faceplusplus.com/static/resources/python_demo/1.jpg'
-  // }, function(err, result) {
-  //   if (err) {
-  //     // TODO handle error
-  //     console.log('error');
-  //     return;
-  //   }
-  //   // TODO use result
-  //   console.log('result', result);
-  //   document.getElementById('response').innerHTML = JSON.stringify(result, null, 2);
-  // });
 });
+
+
+
+
+// below uses data sending method of transferring screenshot.. takes too long
+  // var chunkArr = [];
+  // comm.on('data', function(options) {
+  //   var data = options.data;
+  //   chunkArr.push(data.message);
+  //   if (options.data.last) {
+  //     context2.fillRect(0, 0, width, height);
+  //     context2.drawImage(video2, 0, 0, width, height);
+  //     console.log('chunkarr', chunkArr);
+  //     $('#img2').attr('src', chunkArr.join(''));
+  //   }
+  // })
+  // var chunkLength = 1000;
+
+  // below uses data sending method of transferring screenshot.. takes too long
+      // $('#img1').attr('src', dataUrl);
+      // function onRead (event, text) {
+      //   var data = {}; // obj to transmit over icecomm channel
+      //   if (text.length > chunkLength) {
+      //     data.message = text.slice(0, chunkLength);
+      //   }
+      //   else {
+      //     data.message = text;
+      //     data.last = true;
+      //   }
+      //   comm.send(data);
+      //   var remainingDataUrl = text.slice(data.message.length);
+      //   console.log(data.last, remainingDataUrl.length);
+      //   if (remainingDataUrl.length) {
+      //     setTimeout(function() {
+      //       onRead(null, remainingDataUrl); 
+      //     }, 350);
+      //   }
+      // }
+      // onRead(true, dataUrl);
